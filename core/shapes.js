3f0e1773a8b48f62df5bf338d454fab0cb6043f1
comiratoalex-cmd <comiratoalex@gmail.com>
2025-11-19 15:00:53 +0000

    Update shapes.js

--- core/shapes.js
+++ core/shapes.js
@@ -1,36 +1,37 @@
 function applyShape(type) {
-  const preview = document.getElementById("preview");
+  const p = document.getElementById("preview");
+  const canvas = document.getElementById("pngCanvas");
+
+  canvas.style.display = "none";
 
   if (type === "line") {
-    preview.style.position = "fixed";
-    preview.style.top = "0";
-    preview.style.left = "0";
-    preview.style.width = "100vw";
-    preview.style.height = "12px";
-    preview.style.borderRadius = "0";
+    p.style.position = "fixed";
+    p.style.top = "0";
+    p.style.left = "0";
+    p.style.width = "100vw";
+    p.style.height = "14px";
+    p.style.borderRadius = "0";
   }
 
   if (type === "rectangle") {
-    preview.style.position = "fixed";
-    preview.style.inset = "0";
-    preview.style.height = "100vh";
-    preview.style.width = "100vw";
-    preview.style.borderRadius = "0";
+    p.style.position = "fixed";
+    p.style.top = "0";
+    p.style.left = "0";
+    p.style.width = "100vw";
+    p.style.height = "100vh";
   }
 
   if (type === "ellipse") {
-    preview.style.position = "fixed";
-    preview.style.width = "350px";
-    preview.style.height = "350px";
-    preview.style.borderRadius = "50%";
-    preview.style.left = "50%";
-    preview.style.top = "50%";
-    preview.style.transform = "translate(-50%, -50%)";
+    p.style.position = "fixed";
+    p.style.width = "380px";
+    p.style.height = "380px";
+    p.style.left = "50%";
+    p.style.top = "50%";
+    p.style.transform = "translate(-50%, -50%)";
+    p.style.borderRadius = "50%";
   }
 
   if (type === "png") {
-    document.getElementById("pngCanvas").style.display = "block";
-  } else {
-    document.getElementById("pngCanvas").style.display = "none";
+    canvas.style.display = "block";
   }
 }
