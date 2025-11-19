1d5149956de6e493f57310f387a707b8a20f5a2d
comiratoalex-cmd <comiratoalex@gmail.com>
2025-11-19 02:54:12 +0000

    Update png-border.js

--- core/png-border.js
+++ core/png-border.js
@@ -1,3 +1,15 @@
-function drawPNGBorder(src) {
-  console.log("PNG border:", src);
-}
+pngUpload.addEventListener("change", function () {
+  const file = this.files[0];
+  if (!file) return;
+
+  const img = new Image();
+  img.src = URL.createObjectURL(file);
+
+  img.onload = function () {
+    const canvas = document.getElementById("pngCanvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+  };
+});
