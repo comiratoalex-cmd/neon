cf44eabf45ade66a8ccf80cb06291778fd208fb2
comiratoalex-cmd <comiratoalex@gmail.com>
2025-11-19 02:52:19 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-console.log("Neon Generator Loaded");
-
 function updateBorder() {
-  applyShape(document.getElementById("borderType").value);
-  applyPreset(document.getElementById("presetSelect").value);
+  const preview = document.getElementById("preview");
+
+  const colors = [
+    c1.value, c2.value, c3.value,
+    c4.value, c5.value, c6.value
+  ];
+
+  preview.style.background = `linear-gradient(90deg, ${colors.join(",")})`;
+  preview.style.backgroundSize = "400% 400%";
+
+  applyShape(borderType.value);
   generateOBSLink();
 }
 
-document.querySelectorAll("input,select").forEach(el => {
-  el.addEventListener("input", updateBorder);
-});
+document.querySelectorAll("input,select").forEach(el =>
+  el.addEventListener("input", updateBorder)
+);
 
 updateBorder();
