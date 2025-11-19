5bc3a55da3b8e2bff76b09d26f5f49f4b64a0859
comiratoalex-cmd <comiratoalex@gmail.com>
2025-11-19 02:53:48 +0000

    Update obs-link.js

--- core/obs-link.js
+++ core/obs-link.js
@@ -1,4 +1,9 @@
 function generateOBSLink() {
-  const url = window.location.href + "?obs=1";
+  const url = window.location.origin + window.location.pathname + "?border=1";
   document.getElementById("obsLink").value = url;
 }
+
+copyObs.onclick = () => {
+  obsLink.select();
+  document.execCommand("copy");
+};
