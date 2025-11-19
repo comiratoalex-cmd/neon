72719caeb4748c6447d58c19ae5d28fcc78cbda2
comiratoalex-cmd <comiratoalex@gmail.com>
2025-11-19 02:53:11 +0000

    Update shapes.js

--- core/shapes.js
+++ core/shapes.js
@@ -1,22 +1,36 @@
 function applyShape(type) {
   const preview = document.getElementById("preview");
 
-  preview.className = "";
-
   if (type === "line") {
-    preview.style.height = "20px";
-    preview.style.width = "100%";
+    preview.style.position = "fixed";
+    preview.style.top = "0";
+    preview.style.left = "0";
+    preview.style.width = "100vw";
+    preview.style.height = "12px";
+    preview.style.borderRadius = "0";
   }
+
   if (type === "rectangle") {
     preview.style.position = "fixed";
     preview.style.inset = "0";
+    preview.style.height = "100vh";
+    preview.style.width = "100vw";
+    preview.style.borderRadius = "0";
   }
+
   if (type === "ellipse") {
-    preview.style.width = "400px";
-    preview.style.height = "400px";
+    preview.style.position = "fixed";
+    preview.style.width = "350px";
+    preview.style.height = "350px";
     preview.style.borderRadius = "50%";
+    preview.style.left = "50%";
+    preview.style.top = "50%";
+    preview.style.transform = "translate(-50%, -50%)";
   }
+
   if (type === "png") {
     document.getElementById("pngCanvas").style.display = "block";
+  } else {
+    document.getElementById("pngCanvas").style.display = "none";
   }
 }
